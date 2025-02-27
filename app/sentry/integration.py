17c75646d7176912ad6ab427da379cebba6a253e
FabL-B <errebelneibaf@gmail.com>
2025-02-27 16:16:02 +0100

    feat(sentry): add integration in app folder

---
+++ app/sentry/integration.py
@@ -0,0 +1,10 @@
+import os
+import sentry_sdk
+
+def init_sentry():
+    sentry_sdk.init(
+        dsn=os.getenv("SENTRY_DSN"),
+        traces_sample_rate=1.0,
+        profiles_sample_rate=1.0,
+        environment=os.getenv("ENVIRONMENT", "development")
+    )
